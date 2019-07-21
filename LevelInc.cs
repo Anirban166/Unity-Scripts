3f2df1a3de6be3b49a1bfeaff5eb819b1cc025d3
Anirban Chetia <bloodraven166@gmail.com>
2019-07-21 11:50:59 +0530

    Level Incrementer Script
    Use to change level according to Build Index. Decrement is rarely used, unless a level-recursive game is produced.

---
+++ LevelInc.cs
@@ -0,0 +1,12 @@
+//use build index:
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+  public void startgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
