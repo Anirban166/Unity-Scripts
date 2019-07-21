295f2090691eda2db4e3a37d3e9d87d0d1801d7a
Anirban Chetia <bloodraven166@gmail.com>
2019-07-21 12:04:48 +0530

    Minimap Camera script.
    Minimap follow up based on y-axis.

---
+++ Minimap.cs
@@ -0,0 +1,18 @@
+//Note: Use FollowPlayer script to ensure main camera is following the player.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimap : MonoBehaviour
+{
+    public Transform player;
+    void LateUpdate()
+    {
+        Vector3 newposition = player.position;
+        newposition.y = transform.position.y;    // Only following across y-axis. 
+        transform.position = newposition;
+    }
+}
+
+//Link with minimap camera (set a canvas/UI)
