2710a356a81330302c68167bab327bc9d612009a
Anirban Chetia <bloodraven166@gmail.com>
2019-07-22 08:30:05 +0530

    Game Manager script
    Invokes restart/level-completion after endpoint is reached.

---
+++ GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameend = false;
+    public float resdelay = 2f;
+    public GameObject completelevelUI;
+    public void Endgame()
+    {
+        if (gameend == false)
+        {
+            gameend = true;
+            Debug.Log("The End"); //Remove, test purpose
+            
+            Invoke("Restart",resdelay);
+        }
+
+    }
+    public void CompleteLevel()
+    {
+        completelevelUI.SetActive(true);
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+}
