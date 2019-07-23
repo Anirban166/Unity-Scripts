9710f2005e8ce6cb02e12df9d135c1e541ba77de
Anirban Chetia <bloodraven166@gmail.com>
2019-07-23 08:46:57 +0530

    Score-text Update script
    Uni-directional Movement-triggered Score-text update

---
+++ Score.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player; // Score would change with position of player, Hence take transform component
+    public Text ScoreText; // Create a text object and link field with this script
+
+    // Update is called once per frame
+    void Update()
+    { 
+      ScoreText.text=player.position.z.ToString("0"); // Making initial score zero and would update if player moves along z-axis
+     
+    }
+}
