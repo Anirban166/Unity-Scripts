d20a310a605531d745ff55821307812e44291efb
Anirban Chetia <bloodraven166@gmail.com>
2019-07-25 10:17:15 +0530

    Destructible version of an object

---
+++ Destruct.cs
@@ -0,0 +1,19 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+	public GameObject destroyedVersion;	// Reference to the shattered version of the object
+
+	// If the player clicks on the object
+	void OnMouseDown ()
+	{
+		// Spawn a shattered object
+		Instantiate(destroyedVersion, transform.position, transform.rotation);
+		// Remove the current object
+		Destroy(gameObject);
+	}
+
+}
