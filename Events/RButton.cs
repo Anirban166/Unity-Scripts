ef7afaac93340d703ef24744d40e49145053ceb0
Anirban Chetia <bloodraven166@gmail.com>
2019-11-25 13:15:50 +0530

    Radio Button.

---
+++ Events/RButton.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace WindowsFormsApplication5
+{
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        MessageBox.Show("You Have choosen your desired vehicle using Radio Button ");
+    }
+}
+}
